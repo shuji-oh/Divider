f0b08b6ffa32235ab174d4c139fd51cc60a7dc36
rangosta <rangosta@beagle.naist.jp>
2019-07-26 19:19:06 +0900

    add files

--- machine_learning/prototype/raw2delay.py
+++ machine_learning/prototype/raw2delay.py
@@ -13,25 +13,18 @@ def en(xs):
 params = sys.argv
 sum_delay_list = []
 count = 0
-#with open('panda.dat','r') as f:
-#with open('minato.dat','r') as f:
-#with open('ard1.dat','r') as f:
-#with open('ard2.dat','r') as f:
-#with open('suzuki_ecu.dat','r') as f:
-#with open('prius_meter.dat','r') as f:
 with open(params[1],'r') as f:
     for row in f:
-    	if '[Packet_' == row[0:8]:
+    	if '[packet_' == row[0:8]:
             if count > 2:
-                #print('{:.4f}'.format(mean(sum_delay_list)),'{:.4f}'.format(stdev(sum_delay_list)),'{:.4f}'.format(variance(sum_delay_list)),'{:.4f}'.format(skew(sum_delay_list)),'{:.4f}'.format(kurtosis(sum_delay_list)),'{:.4f}'.format(max(sum_delay_list)),'{:.4f}'.format(rms(sum_delay_list)),'{:.4f}'.format(en(sum_delay_list)))
                 print('{:.4f}'.format(mean(sum_delay_list)),'{:.4f}'.format(stdev(sum_delay_list)),'{:.4f}'.format(variance(sum_delay_list)),'{:.4f}'.format(skew(sum_delay_list)),'{:.4f}'.format(kurtosis(sum_delay_list)),'{:.4f}'.format(max(sum_delay_list)),'{:.4f}'.format(min(sum_delay_list)),'{:.4f}'.format(rms(sum_delay_list)),'{:.4f}'.format(en(sum_delay_list)))
-                #print(mean(sum_delay_list),stdev(sum_delay_list),sum_delay_list)
             sum_delay_list.clear()
             count = 0
     	else :
-            #row_spt = row.split(',')
-            #elapsed_time = int(row_spt[1], 16) * 20
-            elapsed_time = int(row, 16) * 20
+            row_spt = row.split(',')
+            row_id_spt = row_spt[0].split(':')
+            row_time_spt = row_spt[1].split(':')
+            elapsed_time = int(row_time_spt[1], 16) * 20
             ideal_time = int((elapsed_time+500)/2000)*2000
             delay_time = elapsed_time - ideal_time
             sum_delay_list.append(delay_time)
